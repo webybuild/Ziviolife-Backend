955c246844365c418de1855e14a6a8d2c79b0f2b
knlkamal32 <knlkamal32@gmail.com>
2025-12-20 01:41:05 +0530

    subcategory bug fix

--- controller/getSubCategories.js
+++ controller/getSubCategories.js
@@ -6,7 +6,11 @@ module.exports = async (req, res) => {
     try {
         const { categoryName } = req.params;
         const category = await Category.findOne({ where: { name: categoryName}, attributes: ['id']});
-        const data = await SubCategory.findAll({ where: { categoryId: category.dataValues.id }});
+        console.log(category);
+        if(!category) {
+            return res.status(404).json({message: 'Category not found'})
+        }
+        const data = await SubCategory.findAll({ where: { categoryId: category.dataValues.id || category.id, status: 'Active' }});
         res.status(200).json({data})
     } catch (error) {
         console.log(error);
